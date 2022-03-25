6a417ce00df70f17da4270de4b6c27fe0022d6b7
y0014984 <y0014984@gmail.com>
2022-03-25 18:31:43 +0100

    disabled ACE3 default carrying on portable lights

--- addons/interaction/CfgVehicles.hpp
+++ addons/interaction/CfgVehicles.hpp
@@ -6,14 +6,17 @@ class CfgVehicles
 
 	class Land_PortableLight_single_F_AE3: Land_PortableLight_single_F
 	{
+		// Carrying
+		ace_dragging_canCarry = 0;  // Can be carried (0-no, 1-yes)
+
 		// Dragging
-        ace_dragging_canDrag = 1;  // Can be dragged (0-no, 1-yes)
-        ace_dragging_dragPosition[] = {0, 1, 0};  // Offset of the model from the body while dragging (same as attachTo)
-        ace_dragging_dragDirection = 0;  // Model direction while dragging (same as setDir after attachTo)
+		ace_dragging_canDrag = 1;  // Can be dragged (0-no, 1-yes)
+		ace_dragging_dragPosition[] = {0, 1, 0};  // Offset of the model from the body while dragging (same as attachTo)
+		ace_dragging_dragDirection = 0;  // Model direction while dragging (same as setDir after attachTo)
 
 		// Cargo
-        ace_cargo_canLoad = 1;  // Enables the object to be loaded (1-yes, 0-no)
-        ace_cargo_size = 2;  // Cargo space the object takes
+		ace_cargo_canLoad = 1;  // Enables the object to be loaded (1-yes, 0-no)
+		ace_cargo_size = 2;  // Cargo space the object takes
 
 		class AE3_Device
 		{
@@ -43,14 +46,17 @@ class CfgVehicles
 
 	class Land_PortableLight_double_F_AE3: Land_PortableLight_double_F
 	{
+        // Carrying
+        ace_dragging_canCarry = 0;  // Can be carried (0-no, 1-yes)
+		
 		// Dragging
-        ace_dragging_canDrag = 1;  // Can be dragged (0-no, 1-yes)
-        ace_dragging_dragPosition[] = {0, 1, 0};  // Offset of the model from the body while dragging (same as attachTo)
-        ace_dragging_dragDirection = 0;  // Model direction while dragging (same as setDir after attachTo)
+		ace_dragging_canDrag = 1;  // Can be dragged (0-no, 1-yes)
+		ace_dragging_dragPosition[] = {0, 1, 0};  // Offset of the model from the body while dragging (same as attachTo)
+		ace_dragging_dragDirection = 0;  // Model direction while dragging (same as setDir after attachTo)
 
 		// Cargo
-        ace_cargo_canLoad = 1;  // Enables the object to be loaded (1-yes, 0-no)
-        ace_cargo_size = 2;  // Cargo space the object takes
+		ace_cargo_canLoad = 1;  // Enables the object to be loaded (1-yes, 0-no)
+		ace_cargo_size = 2;  // Cargo space the object takes
 
 		class AE3_Device
 		{
@@ -82,12 +88,12 @@ class CfgVehicles
 	{
 		// Carrying
 		ace_dragging_canCarry = 1;  // Can be carried (0-no, 1-yes)
-        ace_dragging_carryPosition[] = {0, 1, 1};  // Offset of the model from the body while dragging (same as attachTo)
-        ace_dragging_carryDirection = 0;  // Model direction while dragging (same as setDir after attachTo)
+		ace_dragging_carryPosition[] = {0, 1, 1};  // Offset of the model from the body while dragging (same as attachTo)
+		ace_dragging_carryDirection = 0;  // Model direction while dragging (same as setDir after attachTo)
 
 		// Cargo
-        ace_cargo_canLoad = 1;  // Enables the object to be loaded (1-yes, 0-no)
-        ace_cargo_size = 1;  // Cargo space the object takes
+		ace_cargo_canLoad = 1;  // Enables the object to be loaded (1-yes, 0-no)
+		ace_cargo_size = 1;  // Cargo space the object takes
 
 		class AE3_Equipment
 		{
@@ -175,13 +181,13 @@ class CfgVehicles
 	class Land_PortableLight_02_double_sand_F_AE3: Land_PortableLight_02_double_sand_F
 	{
 		// Dragging
-        ace_dragging_canDrag = 1;  // Can be dragged (0-no, 1-yes)
-        ace_dragging_dragPosition[] = {0, 1, 0};  // Offset of the model from the body while dragging (same as attachTo)
-        ace_dragging_dragDirection = 0;  // Model direction while dragging (same as setDir after attachTo)
+		ace_dragging_canDrag = 1;  // Can be dragged (0-no, 1-yes)
+		ace_dragging_dragPosition[] = {0, 1, 0};  // Offset of the model from the body while dragging (same as attachTo)
+		ace_dragging_dragDirection = 0;  // Model direction while dragging (same as setDir after attachTo)
 
 		// Cargo
-        ace_cargo_canLoad = 1;  // Enables the object to be loaded (1-yes, 0-no)
-        ace_cargo_size = 2;  // Cargo space the object takes
+		ace_cargo_canLoad = 1;  // Enables the object to be loaded (1-yes, 0-no)
+		ace_cargo_size = 2;  // Cargo space the object takes
 
 		class AE3_Equipment
 		{
@@ -302,13 +308,13 @@ class CfgVehicles
 	class Land_PortableLight_02_quad_sand_F_AE3: Land_PortableLight_02_quad_sand_F
 	{
 		// Dragging
-        ace_dragging_canDrag = 1;  // Can be dragged (0-no, 1-yes)
-        ace_dragging_dragPosition[] = {0, 1, 0};  // Offset of the model from the body while dragging (same as attachTo)
-        ace_dragging_dragDirection = 0;  // Model direction while dragging (same as setDir after attachTo)
+		ace_dragging_canDrag = 1;  // Can be dragged (0-no, 1-yes)
+		ace_dragging_dragPosition[] = {0, 1, 0};  // Offset of the model from the body while dragging (same as attachTo)
+		ace_dragging_dragDirection = 0;  // Model direction while dragging (same as setDir after attachTo)
 
 		// Cargo
-        ace_cargo_canLoad = 1;  // Enables the object to be loaded (1-yes, 0-no)
-        ace_cargo_size = 2;  // Cargo space the object takes
+		ace_cargo_canLoad = 1;  // Enables the object to be loaded (1-yes, 0-no)
+		ace_cargo_size = 2;  // Cargo space the object takes
 
 		class AE3_Equipment
 		{
@@ -495,13 +501,13 @@ class CfgVehicles
 	class Land_PortableDesk_01_sand_F_AE3: Land_PortableDesk_01_sand_F
 	{
 		// Dragging
-        ace_dragging_canDrag = 1;  // Can be dragged (0-no, 1-yes)
-        ace_dragging_dragPosition[] = {0, 1, 0};  // Offset of the model from the body while dragging (same as attachTo)
-        ace_dragging_dragDirection = 90;  // Model direction while dragging (same as setDir after attachTo)
+		ace_dragging_canDrag = 1;  // Can be dragged (0-no, 1-yes)
+		ace_dragging_dragPosition[] = {0, 1, 0};  // Offset of the model from the body while dragging (same as attachTo)
+		ace_dragging_dragDirection = 90;  // Model direction while dragging (same as setDir after attachTo)
 		
 		// Cargo
-        ace_cargo_canLoad = 1;  // Enables the object to be loaded (1-yes, 0-no)
-        ace_cargo_size = 4;  // Cargo space the object takes
+		ace_cargo_canLoad = 1;  // Enables the object to be loaded (1-yes, 0-no)
+		ace_cargo_size = 4;  // Cargo space the object takes
 
 		class AE3_Equipment
 		{
@@ -616,19 +622,19 @@ class CfgVehicles
 	class Land_DeskChair_01_sand_F_AE3: Land_DeskChair_01_sand_F
 	{
 		//Sitting
-        acex_sitting_canSit = 1;  // Enable sitting
+		acex_sitting_canSit = 1;  // Enable sitting
 		acex_sitting_interactPosition[] = {0, 0, 0.3}; 
-        acex_sitting_sitDirection = 180;  // Direction relative to object
-        acex_sitting_sitPosition[] = {0, -0.18, -0.45};  // Position relative to object (may behave weird with certain objects)
+		acex_sitting_sitDirection = 180;  // Direction relative to object
+		acex_sitting_sitPosition[] = {0, -0.18, -0.45};  // Position relative to object (may behave weird with certain objects)
 	
-        // Carrying
-        ace_dragging_canCarry = 1;  // Can be carried (0-no, 1-yes)
-        ace_dragging_carryPosition[] = {0, 1, 1};  // Offset of the model from the body while dragging (same as attachTo)
-        ace_dragging_carryDirection = 180;  // Model direction while dragging (same as setDir after attachTo)
+		// Carrying
+		ace_dragging_canCarry = 1;  // Can be carried (0-no, 1-yes)
+		ace_dragging_carryPosition[] = {0, 1, 1};  // Offset of the model from the body while dragging (same as attachTo)
+		ace_dragging_carryDirection = 180;  // Model direction while dragging (same as setDir after attachTo)
 		
 		// Cargo
-        ace_cargo_canLoad = 1;  // Enables the object to be loaded (1-yes, 0-no)
-        ace_cargo_size = 1;  // Cargo space the object takes
+		ace_cargo_canLoad = 1;  // Enables the object to be loaded (1-yes, 0-no)
+		ace_cargo_size = 1;  // Cargo space the object takes
 	};
 
 	/* ================================================================================ */
