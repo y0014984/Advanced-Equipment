f36d0a2ee7d2f3c00310eabc1f58228c2bdf3a5b
IndigoFox <indifox926@gmail.com>
2023-01-14 23:08:06 -0800

    adds CfgEden text attributes to define cargo name
    Entered name will appear in ACE Cargo List
    "Laptop (Olive) [Laptop 1]"
    See https://github.com/acemod/ACE3/blob/master/addons/cargo/functions/fnc_getNameItem.sqf
    
    Please test/adjust :)

--- addons/armaos/Cfg3DEN.hpp
+++ addons/armaos/Cfg3DEN.hpp
@@ -1,4 +1,55 @@
 class Cfg3DEN
 {
-	
+	class Land_Laptop_03_black_F_AE3 {
+    class AttributeCategories {
+      class AE_Properties {
+        class Attributes {
+          class AE3_Laptop {
+            property = "AE3_Laptop_ACECargoName";
+            control = "Edit";
+            displayName = "ACE Cargo Name";
+            tooltip = "This is the name extension that identifies the object when viewed in ACE Cargo.";
+            // https://github.com/acemod/ACE3/blob/master/addons/cargo/functions/fnc_getNameItem.sqf
+            expression = "_this setVariable ['ace_customName',_value,true];";
+            condition = "true";
+            defaultValue = "Laptop 1";
+          };
+        };
+      };
+    };
+  };
+  class Land_Laptop_03_olive_F_AE3 {
+    class AttributeCategories {
+      class AE_Properties {
+        class Attributes {
+          class AE3_Laptop {
+            property = "AE3_Laptop_ACECargoName";
+            control = "Edit";
+            displayName = "ACE Cargo Name";
+            tooltip = "This is the name extension that identifies the object when viewed in ACE Cargo.";
+            expression = "_this setVariable ['ace_customName',_value,true];";
+            condition = "true";
+            defaultValue = "Laptop 1";
+          };
+        };
+      };
+    };
+  };
+  class Land_Laptop_03_sand_F_AE3 {
+    class AttributeCategories {
+      class AE_Properties {
+        class Attributes {
+          class AE3_Laptop {
+            property = "AE3_Laptop_ACECargoName";
+            control = "Edit";
+            displayName = "ACE Cargo Name";
+            tooltip = "This is the name extension that identifies the object when viewed in ACE Cargo.";
+            expression = "_this setVariable ['ace_customName',_value,true];";
+            condition = "true";
+            defaultValue = "Laptop 1";
+          };
+        };
+      };
+    };
+  };
 };
