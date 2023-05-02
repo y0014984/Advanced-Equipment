6abc184045f031cce9d2a4a83f74499c59cbb985
y0014984 <y0014984@gmail.com>
2023-05-02 10:08:08 +0200

    fixed category typo

--- addons/armaos/CfgFactionClasses.hpp
+++ addons/armaos/CfgFactionClasses.hpp
@@ -4,6 +4,6 @@ class CfgFactionClasses
     class NO_CATEGORY;
     class AE3_armaosModules: NO_CATEGORY
     {
-        displayName = "$STR_AE3_ArmaOS_Config_ArmaOSModulesKategoryDisplayName";
+        displayName = "$STR_AE3_ArmaOS_Config_ArmaOSModulesCategoryDisplayName";
     };
 };
