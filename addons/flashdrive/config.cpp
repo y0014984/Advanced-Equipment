154d758aa05f929949b9e309ea8d3ca7e2cd1ad9
y0014984 <y0014984@gmail.com>
2023-07-11 15:27:40 +0200

    added additional dependencies to flashdrive addon

--- addons/flashdrive/config.cpp
+++ addons/flashdrive/config.cpp
@@ -8,7 +8,7 @@ class CfgPatches
         units[] = { "Land_USB_Dongle_01_F_AE3" };
         weapons[] = {RADIO_WEAPON_LIST_STR(Item_FlashDisk_AE3)};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"A3_Modules_F", "cba_main", "ace_main", "acex_main", "ae3_main"};
+        requiredAddons[] = {"A3_Modules_F", "cba_main", "ace_main", "acex_main", "ae3_main", "ae3_armaos", "ae3_filesystem"};
         author = "y0014984|Wasserstoff";
         VERSION_CONFIG;
     };
